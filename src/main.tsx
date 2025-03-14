76cd7b948657708485337c4c01f61955843293d1
Naufal Rizky <85981933+Falzz1010@users.noreply.github.com>
2025-03-15 06:07:36 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>
-      <App />   
+      <App />    
     </AppProvider>
   </StrictMode>,
 )
